266d306e1f0bb03155a34de47b3791051839e367
Anubhav <anubhav.rajput@tothenew.com>
2023-03-17 09:49:28 +0530

    updated

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import AsideBar from './asideBar/AsideBar'
+import './home.css'
 
 function Home() {
   return (
-    <>
+    <div className='home'>
+      <div className='home-aside'>
+        <AsideBar />
+      </div>
+      <div className='home-contant'>
         <h2>Wellcome To Home Page</h2>
-    </>
+      </div>
+      <div className='home-right-aside'>
+        <AsideBar />
+      </div>
+    </div>
   )
 }
 
---
+++ src/component/asideBar/AsideBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './asidebar.css'
+
+const AsideBar = () => {
+  return (
+    <div className='Asidebar'>AsideBar</div>
+  )
+}
+
+export default AsideBar
